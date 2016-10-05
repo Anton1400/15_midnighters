f38a60531f22538a7d3befd97b3f210541ba6373
Anton1400 <akkulov.anton@gmail.com>
2016-10-05 16:18:31 +0400

    Update seek_dev_nighters.py

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,37 @@
+from datetime import datetime, time
+
+import requests
+
+
+API_URL = 'http://devman.org/api/challenges/solution_attempts/'
+
+
+def get_data(page):
+    req = requests.get(API_URL, params={'page': page})
+    if req.status_code == 200:
+        return req.json()['records']
+
+
 def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+    req = requests.get(API_URL)
+    if req.status_code == 200:
+        page_count = req.json()['number_of_pages']
+        for page in range(page_count):
+            yield get_data(page + 1)
+
+
+def is_midnighter(user):
+    user_datetime = datetime.fromtimestamp(user['timestamp'])
+    return(time(5, 0, 0) > user_datetime.time() > time(0, 0, 0))
+
 
 def get_midnighters():
-    pass
+    midnighters = []
+    for i in load_attempts():
+        midnighters += list(filter(is_midnighter, i))
+    return set(map(lambda a: a['username'], midnighters))
+
 
 if __name__ == '__main__':
-  pass
+    for midnighter in get_midnighters():
+        print(midnighter)
